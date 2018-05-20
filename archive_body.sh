fc38d6756a1c63c0e38f4e2c6dc3bfc2cdad64c0
l07g <39455662+l07g@users.noreply.github.com>
2018-05-20 16:42:11 +0200

    Add files via upload

--- archive_body.sh
+++ archive_body.sh
@@ -2,15 +2,21 @@
 
 scan_dossier(){
 
+
+#scan dossier actuel
 for file in "$1"/*
 do
+	#Si c'est un fichier...
 	if [ -f "$file" ]
 	then
+		#on met dans un fichier
 		cat $file >> archive.txt
 		printf "\n" >> archive.txt
 
+	#Si c'est un dossier...
 	elif [ -d "$file" ]
 	then
+		#On recommence dans le nouveau dossier
 		scan_dossier "$file"
 	fi
 
